46ae79bde1121708fa851dd059a5ef0d11887f12
Mark <q11295@flashaim.com>
2019-06-18 23:59:41 +0800

    mk add webp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,12 +31,14 @@ export class HomeComponent implements OnInit {
 
   carouselArr = [
     {
-      image: 'https://picsum.photos/id/20/800/530',
+      image: 'assets/home/banner01.jpg',
+      imageWebp: 'assets/home/webp/banner01.webp',
       h3: '立即上線，佔盡先機',
       p: '現在加入\n網路的商機都是你的'
     },
     {
-      image: 'https://picsum.photos/id/180/800/530',
+      image: 'assets/home/banner02.jpg',
+      imageWebp: 'assets/home/webp/banner02.webp',
       h3: '增加曝光，提升形象',
       p: 'Google 90以上評分\n適合手機/平板/電腦'
     },
